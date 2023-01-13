b061989918645e5c2ad96134e9c9d2406fec1674
Pietro Gancitano <s303880@studenti.polito.it>
2023-01-13 18:15:31 +0100

    homepage and settings

--- A6/planthug/client/src/App.js
+++ A6/planthug/client/src/App.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
+
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Homepage from './Components/General/Homepage';
 import Test from './Components/Test';
-=======
-import './App.css';
-import { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import dayjs from 'dayjs';
-import {Homescreen} from './components/Homescreen';
-import {PlantNavbar} from './components/PlantNavbar';
-import { Container} from 'react-bootstrap';
-import { MyNavbar } from './components/MyNavbar';
-import { MyPlants } from './components/MyPlants';
 
 let up=[{
   "plantId": "1",
@@ -23,24 +13,11 @@ let up=[{
 }]
 
 function App() {
->>>>>>> a672dd035fee4d4a3be2e3777b432b212ced8383
-
-function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         <Route path='/' element={<Homepage/>}/>
         <Route path='/test' element={<Test/>}/>
-=======
-        <Route path='/' element={<>
-          <MyNavbar title='PLANT HUG'/><Homescreen/><PlantNavbar i1={'bi bi-chat-dots-fill'} i2={'bi bi-lightbulb-fill'} i3={'bi bi-camera-fill'}/></>
-        }/>
-        <Route path='/myplants' element={<>
-          <MyNavbar title='My Plants'/><MyPlants userPlants={up}/><PlantNavbar i1={'bi bi-chat-dots-fill'} i2={'bi bi-lightbulb-fill'} i3={'bi bi-camera-fill'}/></>
-        }/>
-        <Route path='*' element={ <Container/> } />
->>>>>>> a672dd035fee4d4a3be2e3777b432b212ced8383
       </Routes>
     </BrowserRouter>
   );
--- A6/planthug/client/src/Components/General/Homepage.js
+++ A6/planthug/client/src/Components/General/Homepage.js
@@ -1,23 +1,54 @@
-import '../../App.css';
-import { Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import { Container, Offcanvas, Row, Col} from 'react-bootstrap';
+import { useState } from 'react';
 import React from 'react';
 import HMP from './HMP';
 import InfoBox from './info';
-
+import {GearFill} from 'react-bootstrap-icons';
 import PlantNavbar from './PlantNavbar';
 import TitleBar from './Titlebar';
 
+
+
+const SettingsPopup = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return(
+
+    <>   
+        <Row>
+            <Col xs={1} style={{textAlign:'left'}}>
+                <GearFill onClick={handleShow}size={25}/>
+            </Col>
+            <Col xs={10} style={{textAlign:'center'}}>
+                <h1>Plant Hug</h1>
+            </Col>
+        </Row>
+        <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+);}
+    
+    
 
 function Homepage(){
 
     return(
         <Container>
             <br></br>
+        
+            <SettingsPopup/>
+
+
             <div style={{textAlign:'center'}}>
-                <TitleBar name='Plant Hug'/>
             </div>
             <br></br>
             <HMP/>
