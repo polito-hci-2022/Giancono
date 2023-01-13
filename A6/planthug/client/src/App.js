a672dd035fee4d4a3be2e3777b432b212ced8383
RICCARDO LUCIFORA s304813 <s304813@studenti.polito.it>
2023-01-13 17:22:11 +0100

    Started My Plants
    I also modified something in the db. You still cannot access it from the app, tho

--- A6/planthug/client/src/App.js
+++ A6/planthug/client/src/App.js
@@ -6,6 +6,17 @@ import {Homescreen} from './components/Homescreen';
 import {PlantNavbar} from './components/PlantNavbar';
 import { Container} from 'react-bootstrap';
 import { MyNavbar } from './components/MyNavbar';
+import { MyPlants } from './components/MyPlants';
+
+let up=[{
+  "plantId": "1",
+  "name": "Allspice",
+  "customName": "Giorgia",
+  "watered": 1,
+  "repotted": 0,
+  "fertilized": 1
+}]
+
 function App() {
 
   return (
@@ -15,7 +26,7 @@ function App() {
           <MyNavbar title='PLANT HUG'/><Homescreen/><PlantNavbar i1={'bi bi-chat-dots-fill'} i2={'bi bi-lightbulb-fill'} i3={'bi bi-camera-fill'}/></>
         }/>
         <Route path='/myplants' element={<>
-          <MyNavbar title='My Plants'/><PlantNavbar i1={'bi bi-chat-dots-fill'} i2={'bi bi-lightbulb-fill'} i3={'bi bi-camera-fill'}/></>
+          <MyNavbar title='My Plants'/><MyPlants userPlants={up}/><PlantNavbar i1={'bi bi-chat-dots-fill'} i2={'bi bi-lightbulb-fill'} i3={'bi bi-camera-fill'}/></>
         }/>
         <Route path='*' element={ <Container/> } />
       </Routes>
