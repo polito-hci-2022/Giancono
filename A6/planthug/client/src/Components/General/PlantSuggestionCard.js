82d921f8f32580ff64387361b5d9fb0fb28bf505
Luca Scibetta s303976 <s303976@studenti.polito.it>
2023-01-14 19:34:11 +0100

    Added rating modal

--- A6/planthug/client/src/Components/General/PlantSuggestionCard.js
+++ A6/planthug/client/src/Components/General/PlantSuggestionCard.js
@@ -1,9 +1,23 @@
-import { Button, Card } from "react-bootstrap";
-import { StarFill } from "react-bootstrap-icons";
+import { Button, Card, Modal } from "react-bootstrap";
+import { Star, StarFill } from "react-bootstrap-icons";
 import PlantNavbar from "./PlantNavbar";
 import Home from "../Buttons/home";
+import React, { useState } from 'react';
 
 function PlantSuggestionCard(props){
+    const [show, setShow] = useState(false);
+    const [rated, setRated] = useState(false);
+    const [rate, setRate] = useState(0);
+    const handleClose = () => {
+        setRate(0);
+        setShow(false);
+    };
+    const handleConfirm = () => {
+        setRated(true);
+        setShow(false);
+    }
+    const handleShow = () => setShow(true);
+
     return(<>
         <Card className="flex-shrink-0 mx-2" style={{width: '18rem', height: '18rem', borderBlockColor: '#386641', backgroundColor: '#386641', color: 'white'}}>
             <Card.Img style={{width: '100%', height: '66%', overflow: 'hidden', objectFit: 'cover'}} variant="top" src={props.path} />
@@ -11,9 +25,32 @@ function PlantSuggestionCard(props){
             <Card.Body className="d-flex flex-row justify-content-between">
                 <Card.Link disabled><StarFill style={{color: "#386641"}} /></Card.Link>
                 <Card.Link href="#"><Button style={{color: 'black', backgroundColor: '#A7C957', borderWidth: 0, boxShadow: 'none', outline: 'none'}}>See more</Button></Card.Link>
-                <Card.Link className="mx-0"><StarFill style={{color: "yellow"}} /></Card.Link>
+                <Card.Link className="mx-0" onClick={handleShow}><StarFill style={{color: "yellow"}} /></Card.Link>
             </Card.Body>
         </Card>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton style={{backgroundColor: '#A7C957'}}>
+                <Modal.Title>Rate our suggestion</Modal.Title>
+            </Modal.Header>
+            {rated ? <Modal.Body className="d-flex flex-row justify-content-center" style={{backgroundColor: '#A7C957'}}>
+                You have already rated this suggestion!
+            </Modal.Body>:
+            <Modal.Body className="d-flex flex-row justify-content-center" style={{backgroundColor: '#A7C957'}}>
+                <a onClick={() => setRate(1)}>{rate >= 1 ? <StarFill color="yellow" className="mx-1" /> : <Star color="yellow" className="mx-1" />}</a>
+                <a onClick={() => setRate(2)}>{rate >= 2 ? <StarFill color="yellow" className="mx-1" /> : <Star color="yellow" className="mx-1" />}</a>
+                <a onClick={() => setRate(3)}>{rate >= 3 ? <StarFill color="yellow" className="mx-1" /> : <Star color="yellow" className="mx-1" />}</a>
+                <a onClick={() => setRate(4)}>{rate >= 4 ? <StarFill color="yellow" className="mx-1" /> : <Star color="yellow" className="mx-1" />}</a>
+                <a onClick={() => setRate(5)}>{rate >= 5 ? <StarFill color="yellow" className="mx-1" /> : <Star color="yellow" className="mx-1" />}</a>
+            </Modal.Body>}
+            {! rated && <Modal.Footer style={{backgroundColor: '#A7C957'}}>
+                <Button style={{backgroundColor: '#6A994E', borderWidth: 0, boxShadow: 'none', outline: 'none'}} onClick={handleClose}>
+                    Cancel
+                </Button>
+                <Button style={{backgroundColor: '#386641', borderWidth: 0, boxShadow: 'none', outline: 'none'}} onClick={handleConfirm}>
+                    Confirm
+                </Button>
+            </Modal.Footer>}
+        </Modal>
         <PlantNavbar i2={<Home/>}/></>
     )
 }
