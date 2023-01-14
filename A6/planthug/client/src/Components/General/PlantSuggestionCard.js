7cd7b1c55a7757d2b15629365983b4bf04ba830a
Luca Scibetta s303976 <s303976@studenti.polito.it>
2023-01-14 18:39:31 +0100

    Fixed bug

--- A6/planthug/client/src/Components/General/PlantSuggestionCard.js
+++ A6/planthug/client/src/Components/General/PlantSuggestionCard.js
@@ -7,7 +7,7 @@ function PlantSuggestionCard(props){
     return(<>
         <Card className="flex-shrink-0 mx-2" style={{width: '18rem', height: '18rem', borderBlockColor: '#386641', backgroundColor: '#386641', color: 'white'}}>
             <Card.Img style={{width: '100%', height: '66%', overflow: 'hidden', objectFit: 'cover'}} variant="top" src={props.path} />
-            <Card.Title className="text-center mt-1">{props.plantName}</Card.Title>
+            <Card.Title className="text-center mt-1">{props.name}</Card.Title>
             <Card.Body className="d-flex flex-row justify-content-between">
                 <Card.Text><StarFill style={{color: "#386641"}} /></Card.Text>
                 <Card.Link href="#"><Button style={{color: 'white', backgroundColor: '#6A994E', borderWidth: 0, boxShadow: 'none', outline: 'none'}}>See more</Button></Card.Link>
